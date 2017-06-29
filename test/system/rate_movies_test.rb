5f8b598b25e8ea5b5ca9e7145951e656aa439703
Andrew Vos <andrew.vos@gmail.com>
2017-06-29 10:57:59 +0100

    Add devise, bootstrap, and devise-bootstrap-views

---
+++ test/system/rate_movies_test.rb
@@ -0,0 +1,18 @@
+require "application_system_test_case"
+
+class RateMoviesTest < ApplicationSystemTestCase
+  test "user rates a movie" do
+    # user = User.create!
+    # movie = Movie.create!
+    # visit movies_path(movie)
+    # click_on 'Rate 3 star'
+    # rating = Rating.find(user
+    # assert movie.reload.ratin
+
+  end
+  # test "visiting the index" do
+  #   visit rate_movies_url
+  #
+  #   assert_selector "h1", text: "RateMovies"
+  # end
+end
